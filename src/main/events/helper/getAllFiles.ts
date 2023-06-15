a237ff712392886f31a8b0972b7ed1ff2477ebb2
linqirong <609413692@qq.com>
2023-06-15 22:31:59 +0800

    feat:getAllfiles-uniapp

--- src/main/events/helper/getAllFiles.ts
+++ src/main/events/helper/getAllFiles.ts
@@ -1,11 +1,10 @@
 import { lstatSync, readdirSync } from 'fs'
-import { homedir } from 'os'
-import { basename, join, resolve } from 'path'
+import { basename, resolve } from 'path'
 import { PressFile } from '../../../common/types'
 
 export const getAllFiles = () => {
   const paths: any[] = []
-  const targetFolder = join(homedir(), 'Desktop', 'PressNotes')
+  const targetFolder = '/Users/qirong77/Documents/PressNotes'
   // const targetFolder = join(homedir(), 'Desktop', 'press-test')
   const dfs = (path: string, level = 0) => {
     const node: PressFile = {
